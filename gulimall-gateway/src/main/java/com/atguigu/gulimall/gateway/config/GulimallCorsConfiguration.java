925bb8b8ab5c8ec4b510940751616c7582c4af35
hyq19 <10775197514@qq.com>
2023-05-09 10:41:51 +0800

    跨域问题：网址端口之前，有一点不同，两个网址就不能通信了 解决方案：在网关处写一个配置类，告诉通过网关这个网址的请求能够进行跨域

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+//跨域配置
 @Configuration
 public class GulimallCorsConfiguration {
 
