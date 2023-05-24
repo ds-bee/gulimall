bb2cf27dd8857a46ed712e6d57c44eab3e35d0f8
hyq19 <10775197514@qq.com>
2023-05-24 15:16:15 +0800

    接收数据和传输数据的具体格式的问题，那些@RequestMapping这些注解的问题 VO对象：封装请求响应数据的 提供的工具类BeanUtils，复制属性
    这里有stream流和复制字段和使用vo的常用操作
    
    mysql的基本语法
    
    在common里面加入常量类，使用枚举类
    
    //new Query<AttrEntity>().getPage(params)自己写的类型 能够获取分页参数 再传入分页查询的类型              IPage<AttrEntity> page = this.page(new Query<AttrEntity>().getPage(params), wrapper);

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
@@ -47,6 +47,7 @@ public class CategoryBrandRelationController {
     /**
      *  /product/categorybrandrelation/brands/list
      *
+     *
      *  1、Controller：处理请求，接受和校验数据
      *  2、Service接受controller传来的数据，进行业务处理
      *  3、Controller接受Service处理完的数据，封装页面指定的vo
